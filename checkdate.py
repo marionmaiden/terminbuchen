2ec81b6436c627ccec4db6a174ba6b20ae228a01
Mario Freitas <marionmaiden@gmail.com>
2020-07-06 00:19:20 +0200

    Allowing to check multiple offices at same time

--- checkdate.py
+++ checkdate.py
@@ -2,9 +2,12 @@ import requests
 import time
 from bs4 import BeautifulSoup
 from emailsender import EmailSender
+import configparser
+import json
 
-url = "https://service.berlin.de/terminvereinbarung/termin/tag.php?termin=1&anliegen[]=327537&dienstleisterlist=122210,122217,122219,122227&herkunft=http%3A%2F%2Fservice.berlin.de%2Fdienstleistung%2F327537%2F"
+# url = "https://service.berlin.de/terminvereinbarung/termin/tag.php?termin=1&anliegen[]=327537&dienstleisterlist=122210,122217,122219,122227&herkunft=http%3A%2F%2Fservice.berlin.de%2Fdienstleistung%2F327537%2F"
 emailPropertiesFile = "./resources/email.properties"
+
 
 """
 """
@@ -18,40 +21,57 @@ def pretty(arr):
 
 """
 """
-def checkTermin(email):
+def checkTermin(email, config):
     found = False
     termins = ""
 
+    urls = json.loads(config.get("urls", "urlArray"))
+
+    # Loop forever
     while not found:
 
-        page = requests.get(url)
-        soup = BeautifulSoup(page.text, 'html.parser')
+        # Run for each url in the list
+        for url in urls:
 
-        months = soup.find_all(class_="calendar-month-table")
+            # Request page and parse to retrieve the months div
+            page = requests.get(url)
+            soup = BeautifulSoup(page.text, 'html.parser')
+            months = soup.find_all(class_="calendar-month-table")
 
+            # Iterate through each month and check for buchbar dates
+            for month in months:
 
-        for month in months:
+                termin = month.find_all(class_="buchbar")
 
-            termin = month.find_all(class_="buchbar")
-
-            if len(termin) > 0:
-                termins += "<b>" + month.find(class_="month").text + "</b>: " + pretty(termin) + "<br>"
-                found = True
-
-
-        if found:
-            print(termins)
-            email.sendEmail(termins)
-
-        found = False
-        termins = ""
-        print("Termin not found retrying in 100s")
-        time.sleep(100)
+                # Add date to String if there is a termin for it
+                if len(termin) > 0:
+                    termins += "<b>" + month.find(class_="month").text + "</b>: " + pretty(termin) + "<br>"
+                    found = True
 
 
+            # If any date found, print on screen and send e-mail
+            if found:
+                print("===================================")
+                print(url + "<br>" + termins)
+                print("===================================")
+                email.sendEmail("<b>URL:</b>" + url + "<br>" +termins)
 
+            # Clean variables and try other urls after sleep for 5 seconds
+            found = False
+            termins = ""
+            print("Termin not found for url - " + url + " . Trying other urls in 5s")
+            time.sleep(5)
+
+        # No dates found. Retry in 50s for all URLS
+        print("Termin not found for all URLS. Retrying in 50s")
+        time.sleep(50)
+
+
+# Init e-mail module, config parser and start date checker
 def main():
     email = EmailSender(emailPropertiesFile)
-    checkTermin(email)
+    config = configparser.ConfigParser()
+    config.read(emailPropertiesFile)
+    checkTermin(email, config)
 
 if __name__ == "__main__": main()
